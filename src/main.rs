4c1f9f3d034cf33d065d9a8e00da6a6075d5bd34
CodeWithCompound <mathematico.loco@gmail.com>
2025-12-08 17:34:37 +0100

    for and while loop

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,28 @@ fn main() {
     let name = String::from("Alice");
     print_name(&name);
     println!("Not borrowed: {}", name);
+
+    let mut i = 5;
+    let mut v = 199;
+
+    for x in 0..i {
+        println!("{}", x + v);
+    }
+
+    while i != v {
+        println!("i = {}, v = {}", i, v);
+        if i > v {
+            i = i - 1;
+        } else {
+            v = v - 1;
+        }
+    }
+    if i == v {
+        println!("i and v are equal at {}", i);
+    }
+
+    println!("done: i = {}, v = {}", i, v);
 }
+
 
 
